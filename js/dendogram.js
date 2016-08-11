65040bc19e27946d3da5ee29eb453499502064eb
LauFernanda <lafernandar@gmail.com>
2016-08-10 20:39:43 -0500

    Layout is now circular and different classes are in different radious

--- js/dendogram.js
+++ js/dendogram.js
@@ -23,35 +23,23 @@ d3.csv("eco_costa_rica.csv", function(data){
 	
 
 	var width = 2000,
-	    height = 1000;
+	    height = 1000,
+		D2R = Math.PI / 180;
 
 	var force = d3.layout.force()
 	    .nodes(d3.values(nodes))
 	    .links(links)
 	    .size([width, height])
 	    .linkDistance(60)
-	    .charge(-100)
-	    .on("tick", tick)
-	    .gravity(0.01)
+	    .linkStrength(0)
+	    //.charge(-30)
+	    .on("tick", moveToRadial)
+	    //.gravity(10)
 	    .start();
 
 	var svg = d3.select("body").append("svg")
 	    .attr("width", width)
 	    .attr("height", height);
-
-	// Per-type markers, as they don't inherit styles.
-	svg.append("defs").selectAll("marker")
-	    .data(["suit", "licensing", "resolved"])
-	  .enter().append("marker")
-	    .attr("id", function(d) { return d; })
-	    .attr("viewBox", "0 -5 10 10")
-	    .attr("refX", 15)
-	    .attr("refY", -1.5)
-	    .attr("markerWidth", 6)
-	    .attr("markerHeight", 6)
-	    .attr("orient", "auto")
-	  .append("path")
-	    .attr("d", "M0,-5L10,0L0,5");
 
 	var path = svg.append("g").selectAll("path")
 	    .data(force.links())
@@ -73,6 +61,57 @@ d3.csv("eco_costa_rica.csv", function(data){
 	    .attr("y", ".31em")
 	    //.text(function(d) { return d.name; });
 
+	function moveToRadial(e) {
+		path.attr("d", linkArc);
+	  circle.each(function(d,i) { radial(d,i,e.alpha); });
+	  circle
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+	  text.each(function(d,i) { radial(d,i,e.alpha); });
+	  text
+		.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+		
+
+	}
+	
+	function radial(data, index, alpha) {
+		// check out the post
+		// http://macwright.org/2013/03/05/math-for-pictures.html
+		// for more info on how this works
+		var startAngle = 30;
+		if(data.type=="ods")
+			var radius = 100;
+		if(data.type=="fuente")
+			var radius = 200;
+		if(data.type=="datos")
+			var radius = 300;
+		
+		var currentAngle = startAngle + (30 * index);
+		var currentAngleRadians = currentAngle * D2R;
+		// the 500 & 250 are to center the circle we are creating
+		var radialPoint = {
+		  x: 500 + radius * Math.cos(currentAngleRadians),
+		  y: 250 + radius * Math.sin(currentAngleRadians)
+		};
+
+
+		// here we attenuate the effect of the centering
+		// by the alpha of the force layout. 
+		// this gives other forces - like gravity -
+		// to have an effect on the nodes
+		var affectSize = alpha * 0.1;
+
+		// here we adjust the x / y coordinates stored in our
+		// data to move them closer to where we want them
+		// this doesn't move the visual circles yet - 
+		// we will do that in moveToRadial
+		data.x += (radialPoint.x - data.x) * affectSize;
+		data.y += (radialPoint.y - data.y) * affectSize;
+
+
+	}
+
 	// Use elliptical arc path segments to doubly-encode directionality.
 	function tick() {
 	  path.attr("d", linkArc);
@@ -84,7 +123,8 @@ d3.csv("eco_costa_rica.csv", function(data){
 	  var dx = d.target.x - d.source.x,
 	      dy = d.target.y - d.source.y,
 	      dr = Math.sqrt(dx * dx + dy * dy);
-	  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+	  //return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+	  return "M" + d.source.x + "," + d.source.y + "L" + d.target.x + "," + d.target.y;
 	}
 
 	function transform(d) {
